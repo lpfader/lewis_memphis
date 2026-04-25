b222cc5b913b39e88b769a3789d336f814961063
lpfader <lpfader@gmail.com>
2026-04-25 19:44:39 -0400

    floating answers

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,7 +1,13 @@
 console.log("Shapes found:", document.querySelectorAll('.memphis-float').length);
 console.log("JS is loading!");
+
 document.querySelectorAll('.memphis-float').forEach(shape => {
   const speed = 0.3 + Math.random() * 0.4;
+
+  // Random X position between the two banners
+  const x = 200 + Math.random() * (window.innerWidth - 400);
+  shape.style.left = `${x}px`;
+
   let y = Math.random() * window.innerHeight;
 
   function float() {
