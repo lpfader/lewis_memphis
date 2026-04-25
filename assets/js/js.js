f1b8ca15ff07b2dd63cf0f9d1c35663ed434c4f5
lpfader <lpfader@gmail.com>
2026-04-25 19:08:51 -0400

    js stuff

---
+++ assets/js/js.js
@@ -0,0 +1,14 @@
+console.log("JS is loading!");
+document.querySelectorAll('.memphis-float').forEach(shape => {
+  const speed = 0.3 + Math.random() * 0.4;
+  let y = Math.random() * window.innerHeight;
+
+  function float() {
+    y += speed;
+    if (y > window.innerHeight) y = -50;
+    shape.style.transform = `translateY(${y}px)`;
+    requestAnimationFrame(float);
+  }
+
+  float();
+});
