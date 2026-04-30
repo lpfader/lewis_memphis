1ff1a32a0d925bb90bc1fe8f639b5b37731178eb
lpfader <lpfader@gmail.com>
2026-04-30 09:36:11 -0400

    edited floating images more

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,17 +2,19 @@ console.log("Shapes found:", document.querySelectorAll('.memphis-float').length)
 console.log("JS is loading!");
 
 document.querySelectorAll('.memphis-float').forEach(shape => {
-  const speed = 0.3 + Math.random() * 0.4;
+const speed = 0.2 + Math.random() * 1.2;
+
 
   // Random X position between the two banners
   const x = 200 + Math.random() * (window.innerWidth - 400);
   shape.style.left = `${x}px`;
 
-  let y = Math.random() * window.innerHeight;
+let y = Math.random() * window.innerHeight * (Math.random() * 3);
+
 
   function float() {
     y += speed;
-    if (y > window.innerHeight) y = -50;
+    if (y > window.innerHeight) y = -100 - Math.random() * 300;
     shape.style.transform = `translateY(${y}px)`;
     requestAnimationFrame(float);
   }
